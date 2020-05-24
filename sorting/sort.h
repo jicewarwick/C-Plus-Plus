98c3254b150dafd3990fc0b0559c9ad7584ff42e
Ji Ce <jicewarwick@gmail.com>
2020-05-24 18:44:29 +0800

    dealing with several sorts and add gtest unit

---
+++ sorting/sort.h
@@ -0,0 +1,170 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <algorithm>
+#define BEAD(i, j) beads[i * max + j]
+
+void BeadSort(int* arr, int n) {
+    // Find the maximum element
+    int max = *std::max_element(arr, arr + sizeof(arr[0]) * n);
+
+    // allocating memory
+    bool* beads = new bool[max * n];
+    std::fill(beads, beads + sizeof(bool) * max * n, 0);
+
+    // mark the beads
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < arr[i]; j++) BEAD(i, j) = 1;
+
+    for (int j = 0; j < max; j++) {
+        // count how many beads are on each post
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += BEAD(i, j);
+            BEAD(i, j) = 0;
+        }
+
+        // Move beads down
+        for (int i = n - sum; i < n; i++) BEAD(i, j) = 1;
+    }
+
+    // Put sortednluesn arransing beads
+    for (int i = 0; i < n; i++) {
+        int j;
+        for (j = 0; j < max && BEAD(i, j); j++) arr[i] = j;
+    }
+    delete[] beads;
+}
+
+void InsertionSort(int* arr, int n) {
+    int new_val, j;
+    for (int i = 1; i < n; i++) {
+        new_val = arr[i];
+        for (j = i - 1; j >= 0 && new_val < arr[j];) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = new_val;
+    }
+}
+
+void ShellSort(int* arr, int n) {
+    for (int gap = n / 2; gap > 0; gap = gap / 2) {
+        for (int i = gap; i < n; i++) {
+            for (int j = i - gap; j >= 0; j = j - gap) {
+                if (arr[j] < arr[j + gap]) {
+                    break;
+                } else {
+                    std::swap(arr[j], arr[j + gap]);
+                }
+            }
+        }
+    }
+}
+
+void BubbleSort(int* arr, int low, int high) {
+    bool swap_check = true;
+    for (int i = low; (i < high) && (swap_check); i++) {
+        swap_check = false;
+        for (int j = 0; j < high - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap_check = true;
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void SelectinSort(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        int* min_loc = std::min_element(&arr[i], &arr[n]);
+        std::swap(arr[i], *min_loc);
+    }
+}
+
+// Quick Sort
+int Partition(int* arr, int low, int high) {
+    int pivot = arr[high];	// pivot
+    int i = low - 1;		// Index of smaller element
+
+    for (int j = low; j < high; j++) {
+        // If current element is smaller than or equal to pivot
+        if (arr[j] <= pivot) {
+            i++;  // increment index of smaller element
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+void QuickSort(int* arr, int low, int high) {
+    if (low < high) {
+        int p = Partition(arr, low, high);
+
+        QuickSort(arr, low, p - 1);
+        QuickSort(arr, p + 1, high);
+    }
+}
+
+// Merge Sort
+void Merge(int* arr, int l, int m, int r) {
+    int len = r - l + 1;
+    int* newloc = new int[len];
+
+    // Copy data to newloc in order
+    int i = l;
+    int j = m + 1;
+    int k = 0;
+    for (; (i <= m) && (j <= r); ++k) {
+        if (arr[i] <= arr[j]) {
+            newloc[k] = arr[i];
+            ++i;
+        } else {
+            newloc[k] = arr[j];
+            ++j;
+        }
+    }
+
+    std::copy(&arr[i], &arr[m + 1], &newloc[k]);
+    std::copy(&arr[j], &arr[r + 1], &newloc[k]);
+
+    // Copy back & clean up
+    std::copy_n(newloc, len, &arr[l]);
+    delete[] newloc;
+}
+void MergeSort(int* arr, int l, int r) {
+    if (l < r) {
+        int m = (l + r) / 2;
+
+        MergeSort(arr, l, m);
+        MergeSort(arr, m + 1, r);
+
+        Merge(arr, l, m, r);
+    }
+}
+
+// Heap Sort
+void convertHeap(int* arr, int n, int x) {
+    int largest = x;
+    int left = 2 * x + 1;
+    int right = 2 * x + 2;
+
+    if (left < n && arr[left] > arr[largest]) largest = left;
+
+    if (right < n && arr[right] > arr[largest]) largest = right;
+
+    if (largest != x) {
+        std::swap(arr[x], arr[largest]);
+        convertHeap(arr, n, largest);
+    }
+}
+void HeapSort(int* arr, int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) convertHeap(arr, n, i);
+
+    for (int i = n - 1; i >= 0; i--) {
+        std::swap(arr[0], arr[i]);
+        convertHeap(arr, i, 0);
+    }
+}
+
+#endif	// SORT_H
--- sorting/test.cpp
+++ sorting/test.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <random>
 #include <gtest/gtest.h>
 
-#include "bead_sort.h"
-#include "quick_sort.h"
+#include <functional>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include "sort.h"
 
 using std::vector;
 
@@ -16,14 +17,13 @@ public:
         std::uniform_int_distribution<> dis(1, max);
 
         vector<int> output;
-        for (int i = 0; i < n; ++i)
-            output.push_back(dis(gen));
+        for (int i = 0; i < n; ++i) output.push_back(dis(gen));
         return output;
     }
 
     static bool isSorted(const vector<int> input_vec) {
         for (size_t i = 1; i < input_vec.size(); ++i) {
-            if (input_vec[i-1] > input_vec[i]) {
+            if (input_vec[i - 1] > input_vec[i]) {
                 return false;
             }
         }
@@ -41,7 +41,6 @@ TEST(SortingTest, BeadSortTest) {
         ASSERT_TRUE(SortingTest::isSorted(random_number));
     }
 }
-
 TEST(SortingTest, QuickSortTest) {
     int num_test = 10;
     int n = 20;
@@ -52,13 +51,66 @@ TEST(SortingTest, QuickSortTest) {
         ASSERT_TRUE(SortingTest::isSorted(random_number));
     }
 }
-
-
-
-
-
-
-
+TEST(SortingTest, BubbleSortTest) {
+    int num_test = 10;
+    int n = 20;
+    int max = 50;
+    for (int i = 0; i < num_test; ++i) {
+        vector<int> random_number = SortingTest::GenerateRandomInt(n, max);
+        BubbleSort(random_number.data(), 0, n);
+        ASSERT_TRUE(SortingTest::isSorted(random_number));
+    }
+}
+TEST(SortingTest, MergeSortTest) {
+    int num_test = 10;
+    int n = 20;
+    int max = 50;
+    for (int i = 0; i < num_test; ++i) {
+        vector<int> random_number = SortingTest::GenerateRandomInt(n, max);
+        MergeSort(random_number.data(), 0, n - 1);
+        ASSERT_TRUE(SortingTest::isSorted(random_number));
+    }
+}
+TEST(SortingTest, ShellSortTest) {
+    int num_test = 10;
+    int n = 20;
+    int max = 50;
+    for (int i = 0; i < num_test; ++i) {
+        vector<int> random_number = SortingTest::GenerateRandomInt(n, max);
+        ShellSort(random_number.data(), n);
+        ASSERT_TRUE(SortingTest::isSorted(random_number));
+    }
+}
+TEST(SortingTest, InsertionSortTest) {
+    int num_test = 10;
+    int n = 20;
+    int max = 50;
+    for (int i = 0; i < num_test; ++i) {
+        vector<int> random_number = SortingTest::GenerateRandomInt(n, max);
+        InsertionSort(random_number.data(), n);
+        ASSERT_TRUE(SortingTest::isSorted(random_number));
+    }
+}
+TEST(SortingTest, SelectionSortTest) {
+    int num_test = 10;
+    int n = 20;
+    int max = 50;
+    for (int i = 0; i < num_test; ++i) {
+        vector<int> random_number = SortingTest::GenerateRandomInt(n, max);
+        SelectinSort(random_number.data(), n);
+        ASSERT_TRUE(SortingTest::isSorted(random_number));
+    }
+}
+TEST(SortingTest, HeapSortTest) {
+    int num_test = 10;
+    int n = 20;
+    int max = 50;
+    for (int i = 0; i < num_test; ++i) {
+        vector<int> random_number = SortingTest::GenerateRandomInt(n, max);
+        HeapSort(random_number.data(), n);
+        ASSERT_TRUE(SortingTest::isSorted(random_number));
+    }
+}
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
